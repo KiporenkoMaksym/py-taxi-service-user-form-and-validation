6bab5d6de690873c3e00c20228306ce72116acf2
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2026-03-20 21:36:52 +0200

    fixed

--- taxi/urls.py
+++ taxi/urls.py
@@ -43,44 +43,54 @@ urlpatterns = [
         name="manufacturer-delete",
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/",
+    path(
+        "cars/<int:pk>/",
          CarDetailView.as_view(),
          name="car-detail"
     ),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/",
+    path(
+        "cars/<int:pk>/update/",
          CarUpdateView.as_view(),
          name="car-update"
     ),
-    path("cars/<int:pk>/toggle-assing/",
+    path(
+        "cars/<int:pk>/toggle-assing/",
          ToggleAssignCarView.as_view(),
          name="car-toggle-assing"
     ),
-    path("cars/<int:pk>/delete/",
+    path(
+        "cars/<int:pk>/delete/",
          CarDeleteView.as_view(),
          name="car-delete"
     ),
-    path("drivers/",
+    path(
+        "drivers/",
          DriverListView.as_view(),
          name="driver-list"
     ),
-    path("drivers/<int:pk>/",
+    path(
+        "drivers/<int:pk>/",
          DriverDetailView.as_view(),
          name="driver-detail"
     ),
-    path("drivers/create/",
+    path(
+        "drivers/create/",
          DriverCreateView.as_view(),
          name="driver-create"
     ),
-    path("drivers/<int:pk>/update/",
+    path(
+        "drivers/<int:pk>/update/",
          DriverUpdateView.as_view(),
          name="driver-update"
     ),
-    path("drivers/<int:pk>/license/",
+    path(
+        "drivers/<int:pk>/license/",
          DriverLicenseUpdateView.as_view(),
          name="driver-license-update"
     ),
-    path("drivers/<int:pk>/delete/",
+    path(
+        "drivers/<int:pk>/delete/",
          DriverDeleteView.as_view(),
          name="driver-delete"
     ),
